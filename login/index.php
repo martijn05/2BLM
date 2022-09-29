99f2ed3a1bb25c86211e4f5805983cd831b061b5
BrentSchillemans <brent.schillemans@hotmail.com>
2022-09-29 11:52:59 +0200

    Paginastructuur

---
+++ login/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>2BLM | Inloggen</title>
+</head>
+<body>
+  <!-- inloggen -->
+  <!-- registreren -->
+  <!-- paswoord vergeten -->
+  <!-- in aparte divs -->
+</body>
+</html>
