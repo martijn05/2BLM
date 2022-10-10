a644add9e82bf3b1ea2fbac5db74f08ef1674389
BrentSchillemans <brent.schillemans@hotmail.com>
2022-10-10 13:23:59 +0200

    Database localhost

--- includes/Database.php
+++ includes/Database.php
@@ -1,17 +1,57 @@
+<?php
+    function connectWithDatabase()
+    {
+        //credentials
+        $dbServername = "127.0.0.1";
+        $dbUsername = "root";
+        $dbPassword = "root";
+        $dbName = "imslab";
 
-$username = "";
-$email = "";
-$errors = array();
+        //make connection
+        $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
-$servername = "jhtstoveke.be.mysql";
-$username = "jhtstoveke_beimslab";
-$password = "IMSLab5R48B66L";
-$dbname = "jhtstoveke_beimslab";
+        if ($conn == false) {
+            //stop met php code
+            die("Connection failed");
+        }
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+        return $conn;
+    }
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    function closeConnection($connection)
+    {
+        $connection->close();
+    }
+
+    function getQuery($sql)
+    {
+        $conn = connectWithDatabase();
+        $items = mysqli_query($conn, $sql);
+        closeConnection($conn);
+        return $items->fetch_all(MYSQLI_ASSOC);
+    }
+
+    function insertQuery($sql)
+    {
+        $conn = connectWithDatabase();
+        $status = mysqli_query($conn, $sql);
+        closeConnection($conn);
+        return $status;
+    }
+
+// $username = "";
+// $email = "";
+// $errors = array();
+
+// $servername = "jhtstoveke.be.mysql";
+// $username = "jhtstoveke_beimslab";
+// $password = "IMSLab5R48B66L";
+// $dbname = "jhtstoveke_beimslab";
+
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
+
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
