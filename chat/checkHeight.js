3bd69031441ceb71ac05b0bb56e3a3b821fce6f2
lina051102 <lina.stroobants@hotmail.com>
2022-10-22 15:33:43 +0200

    Chat style opgemaakt + js style aangepast zodat chat past in scherm

--- chat/checkHeight.js
+++ chat/checkHeight.js
@@ -6,4 +6,4 @@ var screenHeight = screen.height;
 var chatItemsHeight = screenHeight - 300;
 
 document.getElementById("partners").style.height = chatItemsHeight + "px";
-document.getElementById("divToRefresh").style.height = chatItemsHeight - 60 + "px";
+document.getElementById("divToRefresh").style.height = (chatItemsHeight - 230) + "px";
--- chat/index.php
+++ chat/index.php
@@ -30,7 +30,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <title>Chat</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="../style/chat.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
