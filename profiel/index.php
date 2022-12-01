6c16ccd339721c4b8796377a9f8c8a49af3d488a
lina051102 <lina.stroobants@hotmail.com>
2022-12-01 12:06:41 +0100

    Styling aangepast

--- profiel/index.php
+++ profiel/index.php
@@ -12,6 +12,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <title>2BLM | Mijn acccount</title>
     <link rel="stylesheet" type="text/css" href="../style/navbar.css">
+    <link rel="stylesheet" type="text/css" href="../style/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
